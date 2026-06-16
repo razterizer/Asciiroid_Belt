bd05daa25b2f5cea309e0907e8dc3a3d2aa77f21
Rasmus Anthin <scratty@gmail.com>
2026-06-17 01:05:19 +0200

    build.bat: Using new Core/build.bat version.

--- Asciiroid_Belt/build.bat
+++ Asciiroid_Belt/build.bat
@@ -1,10 +1,12 @@
 @echo off
-call ..\..\lib\Core\build.bat
-
 SET configuration="Release"
 IF "%~1" == "Debug" SET configuration="Debug"
 SET target="x64"
-IF "%~2" == "x86" SET target="x86"
+IF "%~2" == "x86" SET target="Win32"
+
+call ..\..\lib\Core\build.bat %~2
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 msbuild Asciiroid_Belt.sln /p:Configuration=%configuration% /p:Platform=%target%
 
 if %errorlevel% neq 0 (
