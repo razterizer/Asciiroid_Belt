f77f40bcf0bfb3189c9feee9f16af9bee72c8382
razterizer <scratty@gmail.com>
2026-08-04 18:45:31 +0200

    Keep music playback asynchronous

--- Asciiroid_Belt/asciiroid_belt.cpp
+++ Asciiroid_Belt/asciiroid_belt.cpp
@@ -105,7 +105,6 @@ public:
           chip_tune.set_volume_slider(volume_music, min_dB, nl_taper);
           //chip_tune.play_tune();
           chip_tune.play_tune_async();
-          chip_tune.wait_for_completion();
       }
     }
     catch (const std::exception& e)
